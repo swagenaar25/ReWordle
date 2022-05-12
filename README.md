b152ad3ee20bb2f93bef94dace461d5a29064543
swagenaar25 <swagenaar@buckley.org>
2022-05-12 14:51:44 -0700

    Add dutch

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ By default, ReWordle picks words with between 2 and 7 letters.
 To play with a specific number of letters, run using the argument --letters
 ```
 re_wordle_console --letters #
-re_wordle_windowed --letters #
 ```
 where ```#``` is replaced with the number of letters you want.
 Of course, append .exe for Windows compiled versions
@@ -19,3 +18,7 @@ Fantasque Sans Mono, the font used for this project is licensed
 under the **SIL Open Font License** which can be read in the
 attached [fonts/LICENSE.txt](assets/fonts/LICENSE.txt) or online at
 [http://scripts.sil.org/OFL](http://scripts.sil.org/OFL).
+
+The Dutch language file is licensed under
+[assets/NL_LICENSE.txt](assets/NL_LICENSE.txt) or online at
+[https://github.com/OpenTaal/opentaal-wordlist/blob/master/LICENSE.txt](https://github.com/OpenTaal/opentaal-wordlist/blob/master/LICENSE.txt)
--- process_language.py
+++ process_language.py
@@ -1,5 +1,36 @@
-with open("unprocessed_latin.txt", encoding="utf-8") as in_file:
-    with open("processed_latin_1.txt", "w", encoding="utf-8") as out_file:
+bad = "1234567890"
+lowercase = "abcdefghijklmnopqrstuvwxyz\n"
+replacement = {
+    "ö": "o",
+    "è": "e",
+    "ë": "e",
+    "ï": "i",
+    "ü": "u",
+    "é": "e",
+    "ê": "e",
+    "å": "a",
+    "û": "u",
+    "ç": "c"
+}
+with open("dutch_raw.txt", encoding="utf-8") as in_file:
+    with open("assets/lang/nl.txt", "w", encoding="utf-8") as out_file:
         for line in in_file:
             if not (line[0] in "'&-+0123456789=") and line[0].lower() == line[0] and " " not in line and "-" not in line and "." not in line:
-                out_file.write(line)
+                ok = True
+                for letter in line:
+                    if letter in bad:
+                        ok = False
+                        break
+                if ok:
+                    for k in replacement:
+                        v = replacement[k]
+                        line = line.replace(k, v)
+                    ok = True
+                    for letter in line:
+                        if letter not in lowercase:
+                            ok = False
+                            if "'" not in line and "ó" not in line:
+                                print(f"Rejected: [{line}]")
+                            break
+                    if ok:
+                        out_file.write(line)
